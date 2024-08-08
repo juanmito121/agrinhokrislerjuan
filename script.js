55f4d8d057ed8334c74e4aaf20e151652537e29d
Juan Kreuz <108690342+juanmito121@users.noreply.github.com>
2024-08-08 11:07:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "O que pode ser encontrado no campo?",
         alternativas: [
             {
                 texto: "Isso é assustador!",
